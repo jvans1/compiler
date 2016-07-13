e8bd456d9b626c68d484817caa253d14079a84b1
James Vanneman <jvannem@gmail.com>
2016-07-13 11:41:05 +0100

    Little cleanup much to be desired still

--- src/Parser.hs
+++ src/Parser.hs
@@ -112,59 +112,103 @@ compile :: [Token] -> (Maybe Expression, String)
 compile tokens = evalState (runWriterT $ runMaybeT parse) tokens
 
 parse :: Parser Expression
-parse = addition <|> subtraction <|> term
+parse = addition <|> subtraction <|>  multiplication <|> division <|> term
 
 logError :: String -> Parser ()
 logError err = tell (err ++ "\n\n")
 
-subtraction :: Parser Expression
-subtraction = do
+matchLParen :: Parser ()
+matchLParen = do
       list <- get
       case list of
-        (L.LParen:xs) -> do
-          put xs
-          expr1 <- parse
-          list <- get
-          case list of
-            (L.Subtract:xs2) -> do
-              put xs2
-              expr2 <- parse
-              t2 <- get
-              case t2 of
-                (L.RParen:t3) -> do 
-                  put t3
-                  return (BinaryOp Subtract expr1 expr2)
-                (x:xs)         -> do 
-                  logError $ "Invalid token found: " ++ show x ++ ". Expected )"
-                  recover xs
-                  return (BinaryOp Subtract expr1 expr2)
-            _ -> rollBack list 
-        _ -> rollBack list
+        (L.LParen:xs) -> put xs
+        _             -> fail ""
+
+matchDivide :: Parser ()
+matchDivide = do
+      l1 <- get
+      case l1 of
+        (L.Divide:xs2) -> put xs2
+        _              -> fail ""
+
+
+division :: Parser Expression
+division = do
+    list <- get
+    matchLParen
+    expr1 <- parse
+    matchDivide
+    expr2 <- parse
+    list <- get
+    case list of
+      (L.RParen:xs) -> return (BinaryOp Divide expr1 expr2)
+      (x:xs) -> do 
+        logError $ "Invalid token found: " ++ show x ++ ". Expected )"
+        recover xs  >> fail ""
+
+matchMultiply :: Parser ()
+matchMultiply = do
+      l1 <- get
+      case l1 of
+        (L.Multiply:xs2) -> put xs2
+        _              -> fail ""
+
+multiplication :: Parser Expression
+multiplication = do
+    list <- get
+    matchLParen
+    expr1 <- parse
+    matchMultiply
+    expr2 <- parse
+    list <- get
+    case list of
+      (L.RParen:xs) -> return (BinaryOp Multiply expr1 expr2)
+      (x:xs) -> do 
+        logError $ "Invalid token found: " ++ show x ++ ". Expected )"
+        recover xs  >> fail ""
+
+matchSubtract :: Parser ()
+matchSubtract = do
+      l1 <- get
+      case l1 of
+        (L.Subtract:xs2) -> put xs2
+        _              -> fail ""
+
+subtraction :: Parser Expression
+subtraction = do
+    list <- get
+    matchLParen
+    expr1 <- parse
+    matchSubtract
+    expr2 <- parse
+    list <- get
+    case list of
+      (L.RParen:xs) -> return (BinaryOp Subtract expr1 expr2)
+      (x:xs) -> do 
+        logError $ "Invalid token found: " ++ show x ++ ". Expected )"
+        recover xs  >> fail ""
+
+
+matchAdd :: Parser ()
+matchAdd = do
+      l1 <- get
+      case l1 of
+        (L.Add:xs2) -> put xs2
+        _              -> fail ""
 
 addition :: Parser Expression
 addition = do
-      list <- get
-      case list of
-        (L.LParen:xs) -> do
-          put xs
-          expr1 <- parse
-          l1 <- get
-          case l1 of
-            (L.Add:xs2) -> do
-              put xs2
-              expr2 <- parse
-              t2 <- get
-              case t2 of
-                (L.RParen:t3) -> do 
-                  put t3
-                  return (BinaryOp Add expr1 expr2)
-                (x:xs)         -> do 
-                  logError $ "Invalid token found: " ++ show x ++ ". Expected )"
-                  recover xs
-                  return (BinaryOp Add expr1 expr2)
-            _ -> rollBack list 
-        _ -> rollBack list
-
+    list <- get
+    matchLParen
+    expr1 <- parse
+    matchAdd
+    expr2 <- parse
+    list <- get
+    case list of
+      (L.RParen:xs) -> return (BinaryOp Add expr1 expr2)
+      (x:xs) -> do 
+        logError $ "Invalid token found: " ++ show x ++ ". Expected )"
+        recover xs  >> fail ""
 
 rollBack xs = put xs >> fail ""
 
@@ -199,10 +243,3 @@ functionApplication = error "addition"
 
 functionDefinition ::  [Token] -> Maybe Expression
 functionDefinition = error "addition"
-
-multiplication ::  [Token] -> Maybe Expression
-multiplication = error "addition"
-
-division ::  [Token] -> Maybe Expression
-division = error "addition"
-
